217207f165f4d74b6d0ba2758669352a254be6fa
Ilovenyambura <nyamburakaranja4@gmail.com>
2017-05-05 15:28:18 +0300

    add changes to javascript to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-  })
-})
+    $("ul").empty();
+    var number = parseInt($("input#number").val());
+
+//end of user interface
+
+    for (m = 1;m <=number; m++)
+       if ((m % 3 === 0) && (m % 5 === 0)) {
+          $("ul").append("<li>pingpong</li>");
+        } else if (m % 3 === 0) {
+          $("ul").append("<li>ping</li>");
+        } else if (m % 5 === 0) {
+          $("ul").append("<li>pong</li>");
+        } else {
+          $("ul").append("<li>" + m + "</li>");
+        }
+  });
+});
